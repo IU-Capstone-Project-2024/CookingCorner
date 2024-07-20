45c7f7b9957be6b0dcb21ae63da9070aae94d0cd
dsae <ilya_zubkov_1996@mail.ru>
2024-07-20 23:50:01 +0300

    fixed bug with updating user image when user updated it but were dispaying old image

--- frontend/src/components/recipe/steps/step-card.tsx
+++ frontend/src/components/recipe/steps/step-card.tsx
@@ -10,11 +10,11 @@ const StepCard = ({ step, img, description }: StepCardProps) => {
       <h2 className="font-bold">Step {step}</h2>
       <img
         src={
-          img === undefined
+          img === undefined || img === null
             ? "/no_image.svg"
             : `https://storage.yandexcloud.net/cooking-corner-backet/${img}`
         }
-        className={`max-h-[200px] w-full max-w-[300px] rounded-md object-cover ${img !== undefined && "border-2 border-mainBlack"}`}
+        className={`max-h-[200px] w-full max-w-[300px] rounded-md object-cover ${img !== undefined && img !== null && "border-2 border-mainBlack"}`}
       />
       <p className="max-w-[320px] text-center">{description}</p>
     </div>
